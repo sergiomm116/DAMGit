198a175c27cbc70a399979b343e35570ddd6466d
SergioMartinezDAM <sergio.mm116@gmail.com>
2019-12-16 09:01:43 +0100

    prog

---
+++ PROGRAMACION/PRACTICAS PROG/PRUEBAS 04/PruebaPOO.java
@@ -0,0 +1,19 @@
+public class PruebaPOO {
+	
+	int v1;
+	
+	public static void main (String[] args) {
+		int v1=8;
+		int v3=4;
+		
+		System.out.println("main v1: "+v1);
+		System.out.println("main v3: "+v3);
+	}
+	
+	public void nombreMetodo(int v1, long v2, int v3){			//void: No devuelve valor ; return: Devuelve valor
+		
+		System.out.println("metodo v1: "+v1);
+		System.out.println("metodo v3: "+v3);
+	}
+}
+
