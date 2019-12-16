fc5d048f6b5ed5fb42734043d4bd9c0ae2104680
SergioMartinezDAM <sergio.mm116@gmail.com>
2019-12-16 09:27:04 +0100

    poo

--- PROGRAMACION/PRACTICAS PROG/PRUEBAS 04/PruebaPOO.java
+++ PROGRAMACION/PRACTICAS PROG/PRUEBAS 04/PruebaPOO.java
@@ -8,6 +8,15 @@ public class PruebaPOO {
 		
 		System.out.println("main v1: "+v1);
 		System.out.println("main v3: "+v3);
+		
+		PruebaPOO obj1=new PruebaPOO();
+		PruebaPOO obj2=new PruebaPOO();
+		
+		if(obj1==obj2){
+			System.out.println("Son iguales");
+		}else{
+			System.out.println("No son iguales");
+		}
 	}
 	
 	public void nombreMetodo(int v1, long v2, int v3){			//void: No devuelve valor ; return: Devuelve valor
